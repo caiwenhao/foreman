f851016a849db3ad95594f5abb806f9b3cbf55f2
caiwenhao <caiwenhao186@gmail.com>
2015-01-29 14:08:42 +0800

    新增旧日志移除

--- modules/backup/manifests/init.pp
+++ modules/backup/manifests/init.pp
@@ -17,6 +17,12 @@ class backup(
     mode    => '775',
     require => File['/data/sh/backup'],
   }
+  file {"remove_log":
+    content => template('backup/remove_log.erb'),
+    path    => "/data/sh/backup/remove_old_log.sh",
+    mode    => '775',
+    require => File['/data/sh/backup'],
+  }
   cron {backup_all:
     command =>"/bin/bash /data/sh/backup/backup_all.sh > /dev/null 2>&1",
     user    =>root,
@@ -31,5 +37,12 @@ class backup(
     minute  =>0,
     require =>Package["crontabs"];
   }
-
+  cron {remove_log:
+    command => "/bin/bash /data/sh/backup/remove_old_log.sh > /dev/null 2>&1",
+    user    => root,
+    weekday => 1,
+    hour    => 1,
+    minute  => 0,
+    require => Package["crontabs"];
+  }
 }
